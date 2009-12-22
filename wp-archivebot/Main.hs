b57e2a5c446a64882d341c9f3a783c3de36b2081
gwern0 <gwern0@gmail.com>
2009-12-21 19:01:46 -0500

    main.hs: clean up http downloading code
    Ignore-this: 3bab50123aafdc5a1b0cf06279f1bd87
    
    darcs-hash:20091222000146-f7719-d02115e0f80e59e3f170da88c0e47bc28b272157

--- wp-archivebot/Main.hs
+++ wp-archivebot/Main.hs
@@ -1,11 +1,9 @@
 import Control.Concurrent (forkIO)
 import Control.Monad (liftM, forM_)
-import Data.Char (intToDigit)
 import Data.List (isInfixOf, isPrefixOf, foldl', deleteBy, nubBy)
 import Data.Maybe (fromJust)
-import Network.HTTP hiding (port)
-import Network.Stream (ConnError)
-import Network.URI
+import Network.HTTP (getRequest, simpleHTTP, rspBody)
+import Network.URI (escapeURIString, isAllowedInURI)
 import System.Environment (getArgs)
 
 import Text.HTML.TagSoup (parseTags, Tag(TagOpen))
@@ -46,7 +44,7 @@ reader email url = items url >>= go
 
    -- Actually fetch a RSS feed and turn it from String to [RSSItem]
    items :: String -> IO [RSSItem]
-   items rurl = do s <- get' rurl
+   items rurl = do s <- openURL rurl
                    let RSSFeed r = fromJust $ parseFeedString s
                    return $ nubBy matchingTitles $ rssItems $ rssChannel r
 
@@ -63,7 +61,7 @@ archiveBot email ls = liftM uniq (fetchArticleURLs ls) >>= mapM_ (archiveURL ema
 
 -- | Run 'extractURLs' on some page's raw HTML
 fetchArticleURLs ::  String -> IO [String]
-fetchArticleURLs = fmap extractURLs . get'
+fetchArticleURLs = fmap extractURLs . openURL
 
 -- | Use the TagSoup library to extract all the hyperlinks in a page. This is really parsing the HTML,
 -- so hopefully there won't be any spurious links.
@@ -73,28 +71,16 @@ extractURLs arg = [x | TagOpen "a" atts <- parseTags arg, (_,x) <- atts, "http:/
 -- | Webcitation.org is set up so one can archive a url just by doing a request for 'webcitation.org/archive?url=url&email=email'
 -- So it's very easy, given a URL and an email, to archive a page. You don't even need to see what the response was.
 archiveURL :: String -> String -> IO ()
-archiveURL email url = print url' >> ignore (get' url')
+archiveURL email url = print url' >> ignore (openURL url')
               where url' = "http://www.webcitation.org/archive?url=" ++ escapeURIString isAllowedInURI url ++ "&email=" ++ email
 
 -- | Convenient wrapper over the complexity of Network.HTTP. Given a URL, we get the raw HTML. No fuss, no muss.
 -- Of course, this means we paper over a bunch of possible errors and issues, but we've no time for them! There are links to archive!
-get' :: String -> IO String
-get' = get . fromJust . parseURI
- where get :: URI -> IO String
-       get uri = do resp <- simpleHTTP (request uri) >>= handleE (error . show)
-                    case rspCode resp of
-                        (2,0,0) -> return (rspBody resp)
-                        _ -> error (httpError resp)
-           where
-               httpError resp = showRspCode (rspCode resp) ++ " " ++ rspReason resp
-               showRspCode (a,b,c) = map intToDigit [a,b,c]
-
-               request :: URI -> Request String
-               request ur = Request{rqURI=ur, rqMethod=GET, rqHeaders=[], rqBody=""}
-
-               handleE :: Monad m => (ConnError -> m a) -> Either ConnError a -> m a
-               handleE h (Left e) = h e
-               handleE _ (Right v) = return v
+openURL :: String -> IO String
+openURL u = do res <- simpleHTTP $ getRequest u 
+               case res of
+                Left _ -> return ""
+                Right y -> return $ rspBody y
 
 -- | Convenience function. 'forkIO' and 'forM_' demand return types of 'IO ()', but most interesting
 -- IO functions don't return void. So one adds a call to 'return ()'; this just factors it out.
